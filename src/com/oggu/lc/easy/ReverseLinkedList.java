b289534f74d712873ed56f5db471d823b1fb90d3
Bhaskar <bhaskaro@gmail.com>
2020-09-28 13:33:59 -0700

    reverse linked list implementation retested

--- src/com/oggu/lc/easy/ReverseLinkedList.java
+++ src/com/oggu/lc/easy/ReverseLinkedList.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.oggu.lc.easy;
 
 import com.oggu.lc.utils.ListNode;
@@ -8,23 +5,32 @@ import com.oggu.lc.utils.ListNodeUtils;
 
 /**
  * @author Bhaskar
- *
  */
 public class ReverseLinkedList {
 
-	/**
-	 * @param args arguments
-	 */
-	public static void main(String[] args) {
+    /**
+     * @param args arguments
+     */
+    public static void main(String[] args) {
 
-		ListNode head = ListNodeUtils.fillListNode(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
-		ListNodeUtils.printListNode(head, "before reversnig");
-		head = reverseList(head);
-		ListNodeUtils.printListNode(head, "after reversnig");
-	}
+        ListNode head = ListNodeUtils.fillListNode(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+        ListNodeUtils.printListNode(head, "before reversnig");
+        ListNode reverseList = reverseList(head);
+        ListNodeUtils.printListNode(reverseList, "after reversnig");
+    }
 
-	public static ListNode reverseList(ListNode head) {
+    public static ListNode reverseList(ListNode head) {
 
-		return head;
-	}
+        ListNode prev = null;
+        ListNode next;
+
+        while (head != null) {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
 }
